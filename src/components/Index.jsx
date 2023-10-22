29638f458f9343b67a2bb90b200b59b84f82895e
msuryasyahruli <msuryasyahruli12@gmail.com>
2023-10-22 16:08:33 +0700

    add search

--- src/components/Index.jsx
+++ src/components/Index.jsx
@@ -1,21 +1,51 @@
-import React from "react";
+import React, { useState } from "react";
 
 const Home = () => {
+  const [input, setInput] = useState("");
+  const [result, setResult] = useState([]);
+
+  const fetchData = (value) => {
+    fetch("https://blanja-backend-v2.vercel.app/products")
+      .then((response) => response.json())
+      .then((json) => {
+        const result = json.data.filter((product) => {
+          return (
+            product &&
+            product.product_name &&
+            product.product_name.toLowerCase().includes(value)
+          );
+        });
+        setResult(result);
+      });
+  };
+
+  const handleChange = (value) => {
+    setInput(value);
+    fetchData(value);
+  };
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="container">
+      <div className="searchBar">
+        <input
+          className="inputSearch"
+          type="text"
+          placeholder="Search"
+          value={input}
+          onChange={(e) => handleChange(e.target.value)}
+        />
+      </div>
+      {input === "" ? (
+        ""
+      ) : (
+        <div className="searchResult">
+          {result.map((result, id) => (
+            <button className="btnResult" key={id}>
+              {result.product_name}
+            </button>
+          ))}
+        </div>
+      )}
     </div>
   );
 };
